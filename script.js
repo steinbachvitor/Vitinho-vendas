b67695deb4d6349af3c9da7a7baed9db4c627fbd
Vitor Steinbach <131713625+steinbachvitor@users.noreply.github.com>
2024-05-28 09:48:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+document.getElementById("guerraTrono").addEventListener("click", function() {
+    alert("Parabéns! você acaba de adquirir: A Guerra dos Tronos: As Crônicas de Gelo e Fogo, volume 1.");
+});
+document.getElementById("furiaRei").addEventListener("click", function() {
+    alert("Parabéns! você acaba de adquirir: A fúria dos reis: As Crônicas de Gelo e Fogo, volume 2");
+});
+document.getElementById("tormentaEspada").addEventListener("click", function() {
+    alert("Parabéns! você acaba de adquirir: A tormenta de espadas: As Crônicas de Gelo e Fogo, volume 3");
+});
+document.getElementById("festimCorvos").addEventListener("click", function() {
+    alert("Parabéns! você acaba de adquirir: O Festim dos Corvos: As Crônicas de Gelo e Fogo, volume 4");
+});
+document.getElementById("dancaDragoes").addEventListener("click", function() {
+    alert("Parabéns! você acaba de adquirir: A dança dos dragões: As Crônicas de Gelo e Fogo, volume 5");
+});
+document.getElementById("fogoSangue").addEventListener("click", function() {
+    alert("Parabéns! você acaba de adquirir: Fogo & Sangue – Volume 1");
+});
+document.getElementById("ascensaoDragao").addEventListener("click", function() {
+    alert("Parabéns! você acaba de adquirir: A Ascensão do Dragão: Uma história ilustrada da dinastia Targaryen – Volume 1");
+});
+document.getElementById("cavaleiro").addEventListener("click", function() {
+    alert("Parabéns! você acaba de adquirir: O cavaleiro dos Sete Reinos (Edição especial)");
+});
