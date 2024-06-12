188a10b26f4fb3c5c9a509a724f383196d308589
vitor <steinbachvitor1@gmail.com>
2024-06-12 09:58:48 -0300

    refatorando codigo

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
+const mensagemTrono = 'A Guerra dos Tronos: As Crônicas de Gelo e Fogo, volume 1.';
+const mensagemFuriaReis = 'A fúria dos reis: As Crônicas de Gelo e Fogo, volume 2';
+const mensagemTormentaEspadas = 'A tormenta de espadas: As Crônicas de Gelo e Fogo, volume 3';
+const mensagemFestimCorvos = 'O Festim dos Corvos: As Crônicas de Gelo e Fogo, volume 4';
+const mensagemDancaDragoes = 'A dança dos dragões: As Crônicas de Gelo e Fogo, volume 5';
+const mensagemAscensaoDragao = 'A Ascensão do Dragão: Uma história ilustrada da dinastia Targaryen – Volume 1';
+const mensagemFogoSangue = 'Parabéns! você acaba de adquirir: Fogo & Sangue – Volume 1';
+const mensagemCavaleiro = 'O cavaleiro dos Sete Reinos (Edição especial)';
 
-document.getElementById("guerraTrono").addEventListener("click", function() {
-    alert("Parabéns! você acaba de adquirir: A Guerra dos Tronos: As Crônicas de Gelo e Fogo, volume 1.");
-});
-document.getElementById("furiaRei").addEventListener("click", function() {
-    alert("Parabéns! você acaba de adquirir: A fúria dos reis: As Crônicas de Gelo e Fogo, volume 2");
-});
-document.getElementById("tormentaEspada").addEventListener("click", function() {
-    alert("Parabéns! você acaba de adquirir: A tormenta de espadas: As Crônicas de Gelo e Fogo, volume 3");
-});
-document.getElementById("festimCorvos").addEventListener("click", function() {
-    alert("Parabéns! você acaba de adquirir: O Festim dos Corvos: As Crônicas de Gelo e Fogo, volume 4");
-});
-document.getElementById("dancaDragoes").addEventListener("click", function() {
-    alert("Parabéns! você acaba de adquirir: A dança dos dragões: As Crônicas de Gelo e Fogo, volume 5");
-});
-document.getElementById("fogoSangue").addEventListener("click", function() {
-    alert("Parabéns! você acaba de adquirir: Fogo & Sangue – Volume 1");
-});
-document.getElementById("ascensaoDragao").addEventListener("click", function() {
-    alert("Parabéns! você acaba de adquirir: A Ascensão do Dragão: Uma história ilustrada da dinastia Targaryen – Volume 1");
-});
-document.getElementById("cavaleiro").addEventListener("click", function() {
-    alert("Parabéns! você acaba de adquirir: O cavaleiro dos Sete Reinos (Edição especial)");
-});
+alerta('cavaleiro', mensagemCavaleiro);
+alerta('fogoSangue', mensagemFogoSangue);
+alerta('guerraTrono', mensagemTrono);
+alerta('furiaRei', mensagemFuriaReis);
+alerta('tormentaEspada', mensagemTormentaEspadas);
+alerta('festimCorvos', mensagemFestimCorvos);
+alerta('dancaDragoes', mensagemDancaDragoes);
+alerta('ascensaoDragao', mensagemAscensaoDragao);
+
+function alerta(id, mensagem){
+    document.getElementById(id).addEventListener("click", function(){
+        alert('Parabéns! você acaba de adquirir: ' + mensagem);
+    })
+}
